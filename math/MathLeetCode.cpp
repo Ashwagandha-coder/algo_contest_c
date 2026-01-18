6431b5f2f731f5e0165edaed6a5072ade365d32e
Danil M <max.cacz@yandex.ru>
2026-01-18 07:58:11 -0500

    add 728

--- math/MathLeetCode.cpp
+++ math/MathLeetCode.cpp
@@ -2,6 +2,8 @@
 // Created by Motherfucker on 18.01.2026.
 //
 
+#include <vector>
+
 #include "PerfectNumbersH.h"
 
 /**
@@ -32,3 +34,41 @@ bool helper(int num, int sum, int index) {
 bool checkPerfectNumber(int num) {
     return helper(num, 1, 2);
 }
+
+
+
+/**
+ * 728. Self Dividing Numbers
+ */
+
+bool isSelfDividingNumber(int number) {
+    int num = number;
+
+    while (num != 0) {
+        int possible = num % 10;
+        if (possible == 0 || number % possible != 0)
+        {
+            return false;
+        }
+        num /= 10;
+    }
+
+    return true;
+}
+
+
+std::vector<int> selfDividingNumbers(int left, int right) {
+    std::vector<int> result;
+
+    for (int num = left; num <= right; num++)
+    {
+        if (isSelfDividingNumber(num))
+        {
+            result.push_back(num);
+        }
+    }
+
+    return result;
+}
+
+
